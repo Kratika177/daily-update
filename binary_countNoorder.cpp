6014678dd20aa06fad55bad743e8d3f69713bdfb
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-07-28 11:19:09 +0530

    Add files via upload

---
+++ binary_countNoorder.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+int main(){
+            int size;
+            cout<<"Enter the size\n";
+            cin>>size;
+            int a[size];
+			cout<<"Enter the elements of array\n";
+			for(int i=0;i<size;i++){
+				cin>>a[i];
+			}
+			cout<<"Enter the key you want to search\n";
+			int key;
+			cin>>key;
+			int low=0,high=size;
+			if(a[0]<a[1]) { //if array is ascending
+				while(low<=high){
+	   		        int mid=(high+low)/2;
+	   		        if(a[mid]==key){
+	   			        int x=mid;
+	   			        while(a[x-1]==key){
+	   			            x=x-1;
+					        count++;   	
+				        }
+				    int y=mid;
+				    while(a[y+1]==key){
+	   			        y=y+1;
+					    count1++;   	
+				    }
+				    cout<<"total occurance of key is  "<<count+count1+1;
+				    break;
+			        }
+			        else if(a[mid]>key){
+	   			        high=mid-1;
+			        }	   	
+	   		        else if(a[mid]<key){
+	   			        low=mid+1;
+	   		        }
+	   	        }
+			}
+			if(a[0]>a[1]){ //if array is descending
+				while(low<=high){
+	   		        int mid=(high+low)/2;
+	   		        if(a[mid]==key){
+	   			        int x=mid;
+	   			        while(a[x-1]==key){
+	   			            x=x-1;
+					        count++;   	
+				        }
+				        int y=mid;
+				        while(a[y+1]==key){
+	   			            y=y+1;
+					        count1++;   	
+				        }
+				        cout<<"total occurance of key is  "<<count+count1+1;
+				        break;
+			        }
+			        else if(a[mid]>key){
+	   			        low=mid+1;
+			        }	   	
+	   		        else if(a[mid]<key){
+	   			        high=mid-1;
+	   		        }
+	   	        }
+			}
+return 0;
+}
