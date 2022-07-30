a2bc91e065ff7eddda66f6dcf29999cbbc677924
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-07-30 14:04:22 +0530

    Add files via upload

---
+++ frequency sort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+
+int main(){
+	
+	priority_queue <pair<int,int> > pq;
+	int a[]={1,1,1,3,2,2,4}; //k=2
+	map <int,int> mp;
+	map<int,int>:: iterator i;
+	for(int i=0;i<7;i++){
+		mp[a[i]]++;
+	}
+	for(i=mp.begin();i!=mp.end();i++){
+	  
+    	pq.push(make_pair(i->second,i->first));	  
+	
+	}
+    while(pq.size()!=0)
+	{
+		pair<int,int> t=pq.top();
+		for(int i=0;i<t.first;i++){
+		  cout<<t.second<<" ";	
+		}
+		
+		pq.pop();
+
+	}
+	
+	
+return 0;	
+}
