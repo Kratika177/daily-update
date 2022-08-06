a517994770524226459e10c47ed05020e767d94a
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-08-06 10:12:21 +0530

    Add files via upload

---
+++ LCS recursive.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int LCS(string s1,string s2,int n,int m){
+    if(n==0||m==0){
+    	return 0;
+	}
+	if(s1[n-1]==s2[m-1]){
+		return 1+LCS(s1,s2,n-1,m-1);
+	}
+	else{
+		return max(LCS(s1,s2,n,m-1),LCS(s1,s2,n-1,m));
+	}
+}
+int main(){
+	int n=5;
+	int m=4;
+	string s1="abcdeg";
+	string s2="abhcij";
+	int result=LCS(s1,s2,n,m);
+	cout<<"result is "<<result;
+}
