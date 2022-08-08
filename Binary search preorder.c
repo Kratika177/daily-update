cf00a4fa0804b3d3e0daa404c4c154e5b1f553f0
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-08-08 11:43:14 +0530

    Create Binary search preorder.c

---
+++ Binary search preorder.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node{ //Structure of a node
+	int data; // data of the node
+	struct node* left; //left child of a node
+	struct node* right; // right child of a node
+};
+struct node* createNode(int data){ // function to create a new node
+	struct node* n; 
+	n=(struct node*)malloc(sizeof(struct node));
+	n->data=data;
+	n->left=NULL;
+	n->right=NULL;
+}
+void preorder(struct node* root){
+	if(root!=NULL){
+		printf("%d ",root->data);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+int main(){
+	struct node* n1=createNode(4);
+	struct node* n2=createNode(1);
+	struct node* n3=createNode(6);
+	struct node* n4=createNode(5);
+	struct node* n5=createNode(2);
+	//linking root node to its children
+	n1->left=n2;
+	n1->right=n3;
+	n2->left=n4;
+	n2->right=n5;
+	preorder(n1);
+return 0;	
+}
