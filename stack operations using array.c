30d6ee9215db17120f4577eae5141a87a68aed91
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-08-18 14:05:39 +0530

    Create stack operations using array.c

---
+++ stack operations using array.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct stack{
+	int size;
+	int top;
+	int *arr;
+};
+int isempty(struct stack *s){
+	if(s->top==-1){
+	    return 1;	
+	}
+	return 0;
+}
+void push(struct stack *s,int data){
+	if(isfull(s)){
+		printf("Stack overflow\n");
+	}
+	else{
+	    s->top++;
+	    s->arr[s->top]=data;		
+	}
+
+}
+int peek(struct stack *s,int i){
+	return s->arr[s->top-i+1];
+}
+int pop(struct stack *s){
+	if(isempty(s)){
+		printf("stack underflow\n");
+	}
+	else{
+	    int x=s->arr[s->top];
+	    s->top--;
+	    return x;		
+	}
+
+}
+int isfull(struct stack *s){
+	if(s->top==s->size-1){
+	    return 1;	
+	}
+	return 0;
+}
+void display(struct stack *s){
+	while(!isempty(s)){
+		printf("%d\n",s->arr[s->top]);
+		pop(s);
+	}
+}
+int main(){
+	struct stack s;
+	s.size=5;
+	s.top=-1;
+	s.arr = (int *)malloc(s.size * sizeof(int));
+	push(&s,1);
+	push(&s,2);
+	push(&s,3);
+	int x=peek(&s,2);
+	printf("Element at 2 is %d\n",x);
+	printf("Elements of stack- \n");
+	display(&s);
+
+
+return 0;	
+}
