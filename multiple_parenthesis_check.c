46c42370c5a8ce7a4f392e1e1ffa17e3ddc0062b
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-08-19 12:11:54 +0530

    Create multiple_parenthesis_check.c

---
+++ multiple_parenthesis_check.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct stack{
+	int size;
+	int top;
+	char *arr;
+};
+int isempty(struct stack *s){
+	if(s->top==-1){
+	    return 1;	
+	}
+	return 0;
+}
+void push(struct stack *s,char data){
+	if(isfull(s)){
+		printf("Stack overflow\n");
+	}
+	else{
+	    s->top++;
+	    s->arr[s->top]=data;		
+	}
+
+}
+int peek(struct stack *s,char i){
+	return s->arr[s->top-i+1];
+}
+int pop(struct stack *s){
+	if(isempty(s)){
+		printf("stack underflow\n");
+	}
+	else{
+	    int x=s->arr[s->top];
+	    s->top--;
+	    return x;		
+	}
+
+}
+int isfull(struct stack *s){
+	if(s->top==s->size-1){
+	    return 1;	
+	}
+	return 0;
+}
+int check_parenthesis(char *exp){
+	struct stack s;
+	s.size=15;
+	s.top=-1;
+	s.arr = (char *)malloc(s.size * sizeof(char));
+	int i=0;
+	for( i=0;exp[i]!='\0';i++){
+		if(exp[i]=='(' || exp[i]=='['|| exp[i]=='{' ){
+			push(&s,exp[i]);
+		}
+		else if(exp[i]==')'||exp[i]==']'||exp[i]=='}'){
+			if(!isempty(&s) ){
+				char c=pop(&s);
+				if((exp[i]==')'&& c=='(') || (exp[i]=='}'&& c=='{') ||(exp[i]==']'&& c=='[') ){}
+				else{
+					return 0;
+				}	
+			}
+			else{
+				return 0;
+			}
+		}
+	}
+	if(!isempty(&s)){
+		return 0;
+	}
+	else{
+		return 1;
+	}
+} 
+void display(struct stack *s){
+	int i=1;
+	while(i<=s->top+1){
+		printf("%d\n",peek(s,i));
+		i++;
+	}
+}
+int main(){
+	char *exp="{[(8*6)]+(9)}";
+    if(check_parenthesis(exp)){
+    	printf("Balanced\n");
+	}
+	else{
+		printf("Unbalanced\n");
+	}
+    
+
+
+return 0;	
+}
