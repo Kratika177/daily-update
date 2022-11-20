69440af25815e1d0b25d9b8eb19ef4a86d0e4dd2
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-11-19 21:49:27 -0800

    Create Level order traversal.java

---
+++ Level order traversal.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/* 
+    
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	static LinkedList<Node> queue = new LinkedList<>();
+    
+    public static void levelOrder(Node root) {
+        System.out.printf("%s ",root.data);
+        if(root.left!=null) queue.add(root.left);
+        if(root.right!=null) queue.add(root.right);
+        if(queue.isEmpty()) return;
+        levelOrder(queue.remove());
+        
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }	
+}
