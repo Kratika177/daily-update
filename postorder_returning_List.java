b937f8d87c313f841ccba4b10451d4e4a267a24e
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2023-09-11 20:36:08 +0530

    Create postorder_returning_List.java

---
+++ postorder_returning_List.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> l=new ArrayList<>();
+    public List<Integer> postOrder(TreeNode root){
+        
+        if(root!=null){
+            
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            l.add(root.val);
+        }
+        return l;
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        return postOrder(root);
+    }
+}
